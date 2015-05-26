f4a6c993f1a8008e0875c049cfe7d520e2d20896
Danielwhyte <danwhy@gmail.com>
2015-05-26 12:34:40 +0100

    implements import method

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ easily find words in the English dictionary
 
 
 #Features
-* [ ] Import list of words into an array called words
+* [x] Import list of words into an array called words
 * [ ] search through list of words for a string and return list of suggestions
 * [ ] record the string that was searched for (for stats and graphs)
---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var ac = require('../');
+
+assert.equal(typeof ac, 'object');
+
+assert.equal(typeof ac.import, 'function');
+
+console.log('ac.import imports a list of words into memory');
+ac.import(function(words){
+    console.log('words.txt had ' + words.length + ' words in it.');
+    assert.equal(words.length, 354986);
+})
