b560762c2ac757dc4849f225157a9cbe3e0a754a
Danielwhyte <danwhy@gmail.com>
2015-05-26 14:52:59 +0100

    implements searching for a string in an array of words

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ var fs = require('fs');
 var ac = {};
 
 ac.import = function(callback){
+    if (typeof callback !== 'function'){
+        return new Error('callback must be a function');
+    }
     var filename = __dirname + '/words.txt';
     fs.readFile(filename, 'utf-8', function(err, data){
         if (err){
             console.log(err);
         }
         ac.words = data.split('\n');
-        callback(ac.words);
+        return callback(ac.words);
     });
+}
+
+ac.findWord = function(word, callback) {
+    var found = [];
+    for (var i = 0; i < ac.words.length; i++){
+        if(ac.words[i].search(word) === 0){
+            found.push(ac.words[i]);
+        }
+    }
+    console.log(found);
+    callback(null, found);
 }
 
 module.exports = ac;
--- test/test.js
+++ test/test.js
@@ -5,8 +5,20 @@ assert.equal(typeof ac, 'object');
 
 assert.equal(typeof ac.import, 'function');
 
-console.log('ac.import imports a list of words into memory');
+console.log('# ac.import imports a list of words into memory');
 ac.import(function(words){
     console.log('words.txt had ' + words.length + ' words in it.');
     assert.equal(words.length, 354986);
-})
+});
+
+console.log('# attempt to invoke ac.import without a valid callback')
+var error = ac.import('string');
+assert.equal(error.message, 'callback must be a function');
+
+console.log('# ac.findWord finds a string in words array');
+ac.import(function(){
+    ac.findWord('awes', function(err, found){
+        assert.equal(err, null);
+        assert.equal(found.length, 8);
+    });
+});
